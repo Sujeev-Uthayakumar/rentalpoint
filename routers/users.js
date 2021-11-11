85bb9308b4ae312ccce9b4949e7e9bb1c0527104
Sujeev Uthayakumar <sujeev115115@gmail.com>
2021-11-11 18:58:49 -0500

    add register features

--- routers/users.js
+++ routers/users.js
@@ -38,7 +38,75 @@ router.get("/register", (req, res) => {
   res.render("register");
 });
 
-router.get("/users/logout", (req, res) => {
+router.post("/register", (req, res) => {
+  const userDate = new Date(req.body.date);
+  const currentDate = new Date();
+  if (
+    req.body.fname &&
+    req.body.lname &&
+    req.body.country &&
+    req.body.date &&
+    req.body.email &&
+    req.body.password &&
+    req.body.confirm
+  ) {
+    connection().query(
+      "SELECT * FROM accounts WHERE email=?",
+      [req.body.email],
+      function (error, results, fields) {
+        if (results.length > 0) {
+          res.render("register", {
+            errorMessage: "The email is already taken, try another one",
+          });
+        } else if (req.body.password !== req.body.confirm) {
+          res.render("register", {
+            errorMessage: "The passwords don't match, please try again",
+          });
+        } else if (
+          req.body.password.length < 8 ||
+          req.body.confirm.length < 8
+        ) {
+          res.render("register", {
+            errorMessage: "Chosen password must be at least 7 characters ",
+          });
+        } else {
+          connection().query(
+            "INSERT INTO accounts(fname, lname, email, country, birthdate, password) VALUES(?,?,?,?,?,?)",
+            [
+              req.body.fname,
+              req.body.lname,
+              req.body.email,
+              req.body.country,
+              req.body.date,
+              req.body.password,
+            ]
+          ),
+            function (error, results, fields) {
+              if (error) {
+                res.render("register", {
+                  errorMessage: "Something went wrong, try again later",
+                });
+              } else {
+                res.render("register", {
+                  successMessage: "You have successfully registered",
+                });
+              }
+            };
+        }
+      }
+    );
+  } else {
+    res.render("register", {
+      errorMessage: "All input fields must be filled in",
+    });
+  }
+});
+
+router.get("/account", (req, res) => {
+  console.log(req.session);
+});
+
+router.get("/logout", (req, res) => {
   res.send("logout");
 });
 
