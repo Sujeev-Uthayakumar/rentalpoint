90d6a5ade348de78a86b1b52515a972c7ea539ed
Sujeev Uthayakumar <sujeev115115@gmail.com>
2021-11-16 14:35:22 -0500

    add dashboard frontend

--- routers/listings.js
+++ routers/listings.js
@@ -12,7 +12,7 @@ router.get("/listings", (req, res) => {
 
 router.post("/search", (req, res) => {
   connection().query(
-    "SELECT * FROM listings AS full_listings JOIN listing_car ON full_listings.listing_id = listing_car.listing_id  WHERE full_listings.location = ? AND listing_car.seats = ?",
+    "SELECT fname, lname, price,DATE_FORMAT(datecreated, '%Y/%m/%d') AS datecreated, DATE_FORMAT(avaliable_start, '%Y/%m/%d') as avaliable_start, DATE_FORMAT(avaliable_end, '%Y/%m/%d') AS avaliable_end, location, manufacturer, model, car_year, seats, state, price FROM accounts, listings AS full_listings JOIN listing_car ON full_listings.listing_id = listing_car.listing_id  WHERE full_listings.location = ? AND listing_car.seats = ? AND accounts.id = host_id",
     [req.body.locationSearch, req.body.seatSearch],
     function (error, results, fields) {
       res.render("carlistings", {
