ec2bd7b83f2621a719b649bab0f1b8c81b49b887
Sujeev Uthayakumar <sujeev115115@gmail.com>
2021-11-24 06:56:22 -0500

    add completed frontend for accounts

--- routers/users.js
+++ routers/users.js
@@ -16,6 +16,9 @@ router.use(
 );
 
 router.get("/login", (req, res) => {
+  if (req.session.loggedin) {
+    res.redirect("/account");
+  }
   res.render("login", { loggedIn: req.session.loggedin });
 });
 
@@ -48,6 +51,9 @@ router.post("/login", (req, res) => {
 });
 
 router.get("/register", (req, res) => {
+  if (req.session.loggedin) {
+    res.redirect("/account");
+  }
   res.render("register", {
     loggedIn: req.session.loggedin,
   });
@@ -128,6 +134,9 @@ router.post("/register", (req, res) => {
 });
 
 router.get("/seller", (req, res) => {
+  if (req.session.loggedin) {
+    res.redirect("/account");
+  }
   res.render("register-seller", {
     loggedIn: req.session.loggedin,
   });
@@ -218,9 +227,9 @@ router.get("/account", (req, res) => {
     const queries = [
       "SELECT fname, lname, country, email, DATE_FORMAT(birthdate, '%Y/%m/%d') AS birthdate  FROM accounts WHERE email=?",
       "SELECT isSeller FROM verified_accounts, accounts WHERE verified_accounts.user_id = accounts.id AND accounts.email = ?",
-      "SELECT listing_id, host_id, DATE_FORMAT(datecreated, '%Y/%m/%d') AS datecreated, price, DATE_FORMAT(avaliable_start, '%Y/%m/%d') AS start, DATE_FORMAT(avaliable_end,'%Y/%m/%d') AS end, location FROM listings WHERE host_id IN (SELECT user_id FROM verified_accounts,accounts WHERE accounts.email=? AND verified_accounts.user_id = accounts.id)",
-      "SELECT DATE_FORMAT(rented_cars.pickup, '%Y/%m/%d') AS pickup, DATE_FORMAT(rented_cars.dropoff, '%Y/%m/%d') AS dropoff, listing_car.car_year, rented_cars.order_id, accounts.fname, accounts.lname, listings.price, listing_car.manufacturer, listing_car.model, listings.host_id FROM rented_cars JOIN accounts ON accounts.id = rented_cars.buyer_id JOIN listing_car ON listing_car.car_id = rented_cars.car_id JOIN listings ON listings.listing_id = rented_cars.listing_id WHERE rented_cars.buyer_id=?",
-      "SELECT * FROM rented_cars AS full_listings JOIN listing_car ON full_listings.listing_id = listing_car.listing_id JOIN accounts ON full_listings.buyer_id = accounts.id WHERE full_listings.listing_id IN(SELECT listing_id FROM listings WHERE listings.host_id = ?)",
+      "SELECT listings.price, full_listings.state, full_listings.manufacturer, full_listings.model, full_listings.car_year, listings.listing_id, host_id, DATE_FORMAT(datecreated, '%Y/%m/%d') AS datecreated, price, DATE_FORMAT(avaliable_start, '%Y/%m/%d') AS start, DATE_FORMAT(avaliable_end,'%Y/%m/%d') AS end, location FROM listings JOIN listing_car AS full_listings ON listings.listing_id = full_listings.listing_id WHERE host_id IN (SELECT user_id FROM verified_accounts,accounts WHERE accounts.email=? AND verified_accounts.user_id = accounts.id)",
+      "SELECT DATE_FORMAT(rented_cars.datecreated, '%Y/%m/%d') AS datecreated, rented_cars.order_id, DATE_FORMAT(rented_cars.pickup, '%Y/%m/%d') AS pickup, DATE_FORMAT(rented_cars.dropoff, '%Y/%m/%d') AS dropoff, listing_car.manufacturer, listing_car.model, listing_car.car_year, rented_cars.location, accounts.email, accounts.fname, accounts.lname, full_listings.price, rented_cars.price AS total FROM listings AS full_listings JOIN rented_cars ON rented_cars.listing_id = full_listings.listing_id JOIN accounts ON accounts.id = full_listings.host_id JOIN listing_car ON listing_car.car_id = rented_cars.car_id WHERE rented_cars.buyer_id=?",
+      "SELECT DATE_FORMAT(full_listings.datecreated, '%Y/%m/%d') AS purchaseDate, full_listings.price, full_listings.order_id, DATE_FORMAT(full_listings.pickup, '%Y/%m/%d') AS pickup, DATE_FORMAT(full_listings.dropoff, '%Y/%m/%d') AS dropoff, listing_car.manufacturer, listing_car.model, listing_car.car_year, accounts.email, accounts.fname, accounts.lname, listing_car.state, full_listings.location FROM rented_cars AS full_listings JOIN listing_car ON full_listings.listing_id = listing_car.listing_id JOIN accounts ON full_listings.buyer_id = accounts.id WHERE full_listings.listing_id IN(SELECT listing_id FROM listings WHERE listings.host_id = ?) ORDER BY full_listings.order_id DESC",
     ];
     connection().query(
       queries.join(";"),
@@ -232,7 +241,6 @@ router.get("/account", (req, res) => {
         req.session.userid,
       ],
       function (error, results, fields) {
-        console.log(results[4]);
         res.render("account", {
           loggedIn: req.session.loggedin,
           accountDetails: results[0][0],
